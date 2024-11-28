466599e23cbcb07b9e7793f0c67cad5811160103
Islam2718 <mislamhn@gmail.com>
2024-11-28 16:56:49 +0600

    rest api - done

--- PracticeFiles/rest-api-demo-data/controllers/users.controller.js
+++ PracticeFiles/rest-api-demo-data/controllers/users.controller.js
@@ -11,7 +11,7 @@ const getAllUsers = (req, res) => {
 const createUser = (req, res) => {
     const newUser = {
         id: users.length + 1,
-        name: req.body.name,
+        name: req.body.username,
         email: req.body.email,
         password: req.body.password
     };
@@ -26,6 +26,7 @@ const createUser = (req, res) => {
 
 const updateUser = (req, res) => {
     const userid = req.params.id;
+    // console.log('UID: '+userid);
     const { username, email, password } = req.body;
     users
         .filter((user) => user.id === userid)
@@ -42,4 +43,45 @@ const updateUser = (req, res) => {
     });
 };
 
-module.exports = { getAllUsers, createUser, updateUser };
+// get by id 
+const getUserById = (req, res) => {
+    const userId = Number(req.params.id); // Convert userId to a number
+    const user = users.find(user => user.id === userId); // Match based on type and value
+
+    if (!user) {
+        return res.status(404).json({
+            message: 'User not found!',
+            status: 404,
+        });
+    }
+
+    res.status(200).json({
+        message: 'Success!',
+        status: 200,
+        data: user,
+    });
+};
+
+// delete 
+const deleteUserById = (req, res) => {
+    const userId = Number(req.params.id); // Convert userId to a number
+    const userIndex = users.findIndex(user => user.id === userId); // Match based on type and value
+
+    if (userIndex === -1) {
+        return res.status(404).json({
+            message: 'User not found!',
+            status: 404,
+        });
+    }
+
+    const deletedUser = users.splice(userIndex, 1);
+
+    res.status(200).json({
+        message: 'Success!',
+        status: 200,
+        data: deletedUser,
+    });
+};
+
+
+module.exports = { getAllUsers, createUser, updateUser, getUserById, deleteUserById };
